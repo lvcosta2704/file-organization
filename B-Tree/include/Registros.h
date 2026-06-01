6cceae0ba8e10c253be966c071b2975c88018a95
0xs1nr <0xs1nr@proton.me>
2026-06-01 12:56:40 -0300

    Criando a estrutura do projeto da Arvore-B

---
+++ B-Tree/include/Registros.h
@@ -0,0 +1,32 @@
+#ifndef REGISTROS_H
+#define REGISTROS_H
+
+typedef struct parestacao ParEstacao;
+typedef struct busca Busca;
+typedef struct cabecalho Cabecalho;
+typedef struct registro Registro;
+
+// ------ PROTOTIPOS DAS FUNCOES PRINCIPAIS ------
+void criarBin(char* csvName, char* binName);
+void listarRegistros(char *binName);
+void buscarRegistros(char *binName, int N);
+void removerRegistros(char *binName, int N);
+void inserirRegistros(char *binName, int N);
+void atualizarRegistros(char *binName, int N);
+
+// ----------- PROTOTIPOS DOS MODULOS -----------
+Cabecalho iniciarCabecalho();
+void lerCabecalho(FILE *fileBin, Cabecalho *cab);
+void escreverCabecalho(FILE *fileBin, Cabecalho cab);
+void lerRegistro(FILE *fileBin, Registro *reg);
+void escreverRegistro(FILE* fileBin, Registro reg);
+void apagarRegistro(FILE *fileBin, Registro *reg, Cabecalho *cab, int RRN);
+Registro inputRegistro();
+Busca resetarFiltro();
+Busca inputFiltro();
+int comparaFiltro(Busca filtro, Registro reg);
+Busca inputAtualizacoes();
+void atualizar(Registro *reg, Busca atualizacoes);
+void contarEstacoesEPares(FILE *fileBin, Cabecalho *cab);
+
+#endif
