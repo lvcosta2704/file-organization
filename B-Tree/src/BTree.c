3d197edff57e93b189d125845c237f14a4b55639
0xs1nr <0xs1nr@proton.me>
2026-06-06 12:30:27 -0300

    Corrigindo a funcao criarNo()

--- B-Tree/src/BTree.c
+++ B-Tree/src/BTree.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "BTree.h"
-#include "query.h"
-#include "utils.h"
+#include "BTree_utils.h"
 
 // === ARVORE-B ===
 
 // Cria um nó vazio
-// Funcionamento:
 // Aloca memória para a criacao do no e inicializa seus campos
 // conforme especificado
 BTreeNode *criarNo() {
@@ -25,17 +23,34 @@ BTreeNode *criarNo() {
   node->tipoNo = -1; // Inicializa o nó como raiz
   node->nroChaves = 0;
 
-  // Inicializa as chaves como -1
+  // Aloca memoria para a array de indices
+  node->indice = malloc(sizeof(struct Indice) * MAX_CHAVES);
+  if(!(node->indice)) {
+    printf("Erro ao alocar memoria.\n");
+    return NULL;
+  }
+
+  // Inicializa os indices como -1
   for (int i = 0; i < MAX_CHAVES; i++) {
     node->indice[i].codEstacao = -1;
     node->indice[i].RRN = -1;
   }
 
-  // Inicializa os indices para os outros nós
-  // como -1
+  // Aloca memoria para a referencia para os outros nós
+  node->ponteiroNo = malloc(sizeof(int) * ORDEM);
+  if(!(node->ponteiroNo)) {
+    printf("Erro ao alocar memoria.\n");
+    return NULL;
+  }
+
+  // Inicializa os indices para os outros nós como -1
   for (int i = 0; i < ORDEM; i++) {
     node->ponteiroNo[i] = -1;
   }
 
   return node;
+}
+
+void buscaArvoreB() {
+  
 }
